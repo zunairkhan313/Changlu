ad0e355647499f80aa4f2185ea3db852076163be
Zunair Khan <121635619+zunairkhan313@users.noreply.github.com>
2024-05-27 05:24:20 -0700

    recommit

--- app/Components/Banner.jsx
+++ app/Components/Banner.jsx
@@ -1,9 +1,9 @@
 "use client"
 
 import Carousel from 'react-bootstrap/Carousel';
-import Banner1 from "../../public/Images/banner1.png"
-import Banner2 from "../../public/Images/banner2.jpg"
-import Banner3 from "../../public/Images/banner3.jpg"
+import Banner1 from "../../public/Images/Banner1.png"
+import Banner2 from "../../public/Images/Banner2.jpg"
+import Banner3 from "../../public/Images/Banner3.jpg"
 import Image from 'next/image';
 
 export default function CarouselPage() {
